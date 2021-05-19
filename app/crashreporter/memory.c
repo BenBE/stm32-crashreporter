4c491a1a7dac03f17bc10e3d808acb73b8c316bf
Benny Baumann <baumann@4h-jena.de>
2021-05-19 15:12:19 +0200

    Special handling for .text, .data and .bss

--- app/crashreporter/memory.c
+++ app/crashreporter/memory.c
@@ -2,6 +2,12 @@
 
 #include "crashreporter/macros.h"
 
+extern void* _etext;
+extern void* _sdata;
+extern void* _edata;
+extern void* _sbss;
+extern void* _ebss;
+
 bool cr_mm_inRegion(const void* ptr, const memory_region_t* region) {
     if(ptr < region->offset) {
         return false;
@@ -14,10 +20,44 @@ bool cr_mm_inRegion(const void* ptr, const memory_region_t* region) {
     return true;
 }
 
+memory_region_t reg_text, reg_data, reg_bss;
+
 memory_region_t const * cr_mm_getRegion(const void* ptr)
 {
     memory_region_t const *result = NULL;
 
+    // Special handling for .text, .data and .bss
+
+    if((char*)ptr >= (char*)(0xFF000000 & (uintptr_t)(char*)&_etext) && (char*)ptr < (char*)&_etext) {
+        reg_text = (memory_region_t) {
+            .offset = (char*)(0xFF000000 & (uintptr_t)(char*)&_etext),
+            .length = ~0xFF000000 & (uintptr_t)(char*)&_etext,
+            .flags = MF_READ | MF_EXEC | MF_ATOMIC | MF_PERSIST,
+            .name = ".text",
+            .bitband_source = NULL
+        };
+        return &reg_text;
+    } else if((char*)ptr >= (char*)&_sdata && (char*)ptr < (char*)&_edata) {
+        reg_data = (memory_region_t) {
+            .offset = &_sdata,
+            .length = (char*)&_edata - (char*)&_sdata,
+            .flags = MF_READ | MF_WRITE | MF_ATOMIC,
+            .name = ".data",
+            .bitband_source = NULL
+        };
+        return &reg_data;
+    } else if((char*)ptr >= (char*)&_sbss && (char*)ptr < (char*)&_ebss) {
+        reg_bss = (memory_region_t) {
+            .offset = &_sbss,
+            .length = (char*)&_ebss - (char*)&_sbss,
+            .flags = MF_READ | MF_WRITE | MF_ATOMIC,
+            .name = ".bss",
+            .bitband_source = NULL
+        };
+        return &reg_bss;
+    }
+
+    // Now for normal section info ...
     for(size_t i = 0; memory_map[i].name; i++) {
         memory_region_t const *curr = &memory_map[i];
 
